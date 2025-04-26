eb8d145a08b22d9029cbe925fd3111eea1db33e7
FranciscoTorres <juanfrancisco87@hotmail.com>
2025-04-26 00:55:29 -0500

    Adding initial core code

---
+++ app.js
@@ -0,0 +1,12 @@
+import './components/a_constants';
+import './components/a_firebaseConfig';
+import './components/ab_base';
+import './components/signUp';
+import './components/resendSignUpEmail';
+import './components/signIn';
+import './components/signOut';
+import './components/accountPage';
+import './components/pressPage';
+import './components/supplierPage';
+import './components/adminPage';
+import './components/companiesPage'; 
